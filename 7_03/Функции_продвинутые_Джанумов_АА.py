8088fa258db8ae7922290ac8bfc5df9f1dd80812
superch1337 <www.alex1269@yandex.ru>
2026-03-11 15:00:29 +0300

    upd4

--- 7_03/Функции_продвинутые_Джанумов_АА.py
+++ 7_03/Функции_продвинутые_Джанумов_АА.py
@@ -62,4 +62,40 @@ assign_table(2,'Arwa', True)
 
 assign_and_print_order(2,'Стейк','Морской окунь','Бутылка вина')
 
+
+#task 4
+
+tables = {
+1: {
+'name': 'Chioma',
+'vip_status': False,
+'order': {
+'drinks': 'Orange Juice, Apple Juice',
+'food_items': 'Pancakes'
+}
+},
+2: {},
+3: {},
+4: {},
+5: {},
+6: {},
+7: {},
+}
+
+def assign_food_items(**order_items):
+    food = order_items.get('food')
+    drinks = order_items.get('drinks')
     
+
+assign_food_items(food='Pancakes, Poached Egg', drinks='Water')
+
+# task 5
+
+def calculate_price_per_person(total, tip, split):
+    total_tip = total * (tip/100)
+    split_price = (total + total_tip) / split
+    print(split_price)
+
+table_7_total = [534.50, 20.0, 5]
+
+calculate_price_per_person(*table_7_total)
