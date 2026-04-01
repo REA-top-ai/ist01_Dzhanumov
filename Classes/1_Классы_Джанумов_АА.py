146e70c1b2aa9801c08f11bf4e7315a1e5b20a37
superch1337 <www.alex1269@yandex.ru>
2026-04-01 14:43:58 +0300

    upd2 Classes

--- Classes/1_Классы_Джанумов_АА.py
+++ Classes/1_Классы_Джанумов_АА.py
@@ -23,3 +23,41 @@ class Circle:
     def area(self,radius):
         return self.pi * radius ** 2
     
+# task 3
+
+
+class Circle:
+    
+    def __init__(self,diameter):
+        print(f'New circle with diameter: {diameter}')
+
+    pi = 3.14
+
+    def area(self,radius):
+        return self.pi * radius ** 2
+    
+
+teaching_table = Circle(36)
+
+# task 4
+
+class Circle:
+    
+    def __init__(self,diameter: int):
+        print(f'New circle with diameter: {diameter}')
+        self.radius = diameter / 2
+    pi = 3.14
+
+    def area(self,radius):
+        return self.pi * radius ** 2
+    
+    def circumference(self):
+        return 2 * self.pi * self.radius
+    
+medium_pizza = Circle(12)
+teaching_table = Circle(36)
+round_room = Circle(11460)
+
+print(medium_pizza.circumference())
+print(teaching_table.circumference())
+print(round_room.circumference())
